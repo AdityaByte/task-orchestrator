26ba4606185338c286e2ab0caa27e5148a78c874
AdityaByte <adityapawar4914@gmail.com>
2025-12-29 22:40:36 +0530

    cli: Implemented CLI for task-engine

---
+++ task_engine/cli/main.py
@@ -0,0 +1,16 @@
+from task_engine.cli.parser import parse_args
+from task_engine.cli.runner import run
+import sys
+
+def main():
+    try:
+        args = parse_args()
+        run(args)  # Running the workflow via cli through the run method.
+    except Exception as e:
+        print(f"[ERROR] {e}")
+        sys.exit(0)
+
+    sys.exit(0)
+
+if __name__ == "__main__":
+    main()
